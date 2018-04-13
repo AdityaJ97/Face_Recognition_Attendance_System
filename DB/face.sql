b01a4ceac6ece95fa93a46eaeee0a21720b88d53
ABHISHEK-JADHAV <jadhavas15.comp@coep.ac.in>
2018-04-13 17:20:53 +0530

    New DB

--- DB/face.sql
+++ DB/face.sql
@@ -1,38 +1,557 @@
-create database FACE;
-use FACE;
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 13, 2018 at 05:20 PM
+-- Server version: 5.5.59-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.24
 
-create table SE_1 (
-	`mis` varchar(10) not null,
-	`1`  int default 0,
-	`2`  int default 0,
-	`3`  int default 0,
-	`4`  int default 0,
-	`5`  int default 0,
-	`6`  int default 0,
-	`7`  int default 0,
-	`8`  int default 0,
-	`9`  int default 0,
-	`10`  int default 0,
-	`11`  int default 0,
-	`12`  int default 0,
-	`13`  int default 0,
-	`14`  int default 0,
-	`15`  int default 0,
-	`16`  int default 0,
-	`17`  int default 0,
-	`18`  int default 0,
-	`19`  int default 0,
-	`20`  int default 0,
-	`21`  int default 0,
-	`22`  int default 0,
-	`23`  int default 0,
-	`24`  int default 0,
-	`25`  int default 0,
-	`26`  int default 0,
-	`27`  int default 0,
-	`28`  int default 0,
-	`29`  int default 0,
-	`30`  int default 0,
-	`31`  int default 0,
-	primary key(`mis`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `FACE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_1`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_1` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_2`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_2` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_3`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_3` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_4`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_4` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `SE_4`
+--
+
+INSERT INTO `SE_4` (`mis`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`, `26`, `27`, `28`, `29`, `30`, `31`) VALUES
+('111503027', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_5`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_5` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_6`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_6` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_7`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_7` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_8`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_8` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_9`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_9` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_10`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_10` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_11`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_11` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SE_12`
+--
+
+CREATE TABLE IF NOT EXISTS `SE_12` (
+  `mis` varchar(10) NOT NULL,
+  `1` int(11) DEFAULT '0',
+  `2` int(11) DEFAULT '0',
+  `3` int(11) DEFAULT '0',
+  `4` int(11) DEFAULT '0',
+  `5` int(11) DEFAULT '0',
+  `6` int(11) DEFAULT '0',
+  `7` int(11) DEFAULT '0',
+  `8` int(11) DEFAULT '0',
+  `9` int(11) DEFAULT '0',
+  `10` int(11) DEFAULT '0',
+  `11` int(11) DEFAULT '0',
+  `12` int(11) DEFAULT '0',
+  `13` int(11) DEFAULT '0',
+  `14` int(11) DEFAULT '0',
+  `15` int(11) DEFAULT '0',
+  `16` int(11) DEFAULT '0',
+  `17` int(11) DEFAULT '0',
+  `18` int(11) DEFAULT '0',
+  `19` int(11) DEFAULT '0',
+  `20` int(11) DEFAULT '0',
+  `21` int(11) DEFAULT '0',
+  `22` int(11) DEFAULT '0',
+  `23` int(11) DEFAULT '0',
+  `24` int(11) DEFAULT '0',
+  `25` int(11) DEFAULT '0',
+  `26` int(11) DEFAULT '0',
+  `27` int(11) DEFAULT '0',
+  `28` int(11) DEFAULT '0',
+  `29` int(11) DEFAULT '0',
+  `30` int(11) DEFAULT '0',
+  `31` int(11) DEFAULT '0',
+  PRIMARY KEY (`mis`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DELIMITER $$
+--
+-- Events
+--
+CREATE DEFINER=`root`@`localhost` EVENT `Clear` ON SCHEDULE EVERY 1 YEAR STARTS '2018-07-01 00:00:00' ENDS '2028-07-01 00:00:00' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'Clear db after every one year' DO BEGIN
+delete from `SE_1` where 1;
+delete from `SE_2` where 1;
+delete from `SE_3` where 1;
+delete from `SE_4` where 1;
+delete from `SE_5` where 1;
+delete from `SE_6` where 1;
+delete from `SE_7` where 1;
+delete from `SE_8` where 1;
+delete from `SE_9` where 1;
+delete from `SE_10` where 1;
+delete from `SE_11` where 1;
+delete from `SE_12` where 1;
+END$$
+
+DELIMITER ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
