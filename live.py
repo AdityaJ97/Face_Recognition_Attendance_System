a19614af31ad1402b926e67f7e45bd3b4dcf81d4
ABHISHEK-JADHAV <jadhavas15.comp@coep.ac.in>
2018-04-15 02:23:13 +0530

    live.py optimized

--- live.py
+++ live.py
@@ -82,18 +82,13 @@ hrs = {
 
 process_this_frame = True
 while True:
-    # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
     if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
@@ -101,11 +96,9 @@ while True:
 	mis = []
 
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance = 0.5)
             name = "Unknown"
 	    misone = ""
-            # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
@@ -115,14 +108,11 @@ while True:
 
     process_this_frame = not process_this_frame
 
-    # Display the results
     for (top, right, bottom, left), name, misone in zip(face_locations, face_names, mis):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
-        # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 	tempt = time.time()
 	month = int(datetime.datetime.fromtimestamp(tempt).strftime('%m'))
@@ -142,7 +132,6 @@ while True:
 		elif tempt - ts[misone] > 10 :
 			while tempv > 10:
 				ts[misone] = 0
-				#hrs to 0
 				print tt[d][start]
                 		print "UPDATE " + tt[d][start] + "_" + str(month) + " SET `" + str(day) + "` = `" + str(day) +"` + 1 where mis= '" + misone + "'"
 				cur.execute("UPDATE " + tt[d][start] +"_" + str(month) + " SET `" + str(day) + "` = `" + str(day) +"` + 1 where mis= '" + misone + "'")
@@ -150,23 +139,14 @@ while True:
 				tempv -= 10
 				start += 1
 		
-        # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-	#print "UPDATE attendance SET record = 1 where mis=" + misone
-#	if name != "unknown":
-		#cur.execute("UPDATE attendance SET record = 1 where mis= '" + misone + "'")
-		#db.commit()
     	
-    # Display the resulting image
 #    cv2.imshow('Video', frame)
-    #cv2.waitKey(1)
-    # Hit 'q' on the keyboard to quit!
 #    if cv2.waitKey(33) & 0xFF == ord('q'):
 #        break
 
 db.close()
-# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
